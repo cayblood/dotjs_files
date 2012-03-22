6e022acb9d8c65db3fea8e50d1c6ba7b226b4fcc
Carl Youngblood <carl@youngbloods.org>
2012-03-22 13:00:27 +0100

    Modified scrumy.com script

--- scrumy.com.js
+++ scrumy.com.js
@@ -1,13 +1,20 @@
-var lanes = ['todo', 'inprogress', 'verify', 'done'];
-$(lanes).each(function(index, lane) {
-  var sum = 0;
-  $('td[id$="' + lane + '"]').each(function(index, arg) {
-    var contents = $(arg).html();
-    var re = /\((\d+)h\)/g;
-    var match;
-    while (match = re.exec(contents)) {
-      sum += parseInt(match[1]);
-    }
-  });
-  $('#' + lane + '-header span:first').append(' (' + sum + 'h)');
+jQuery(document).ready(function() { 
+  function displayColumnSums() {
+    var lanes = {'todo': 'To Do', 'inprogress': 'In Progress', 'verify': 'Verify', 'done': 'Done'};
+    $.each(lanes, function(lane, lane_title) {
+      var sum = 0;
+      $('td[id$="' + lane + '"]').each(function(index, arg) {
+        var contents = $(arg).html();
+        var re = /\((\d+)h\)/g;
+        var match;
+        while (match = re.exec(contents)) {
+          sum += parseInt(match[1]);
+        }
+      });
+      $('#' + lane + '-header span:first').html(lane_title + ' (' + sum + 'h)');
+    });
+  };
+    
+  displayColumnSums();
+  t = setInterval(displayColumnSums, 2000);
 });
